eb2d1ba21f49b49358f01e13a439682bf5e610dc
John Chapman <superjohnnysako@gmail.com>
2023-07-15 07:31:56 -0400

    the beginning of a plan

--- src/plan.py
+++ src/plan.py
@@ -1,6 +1,29 @@
+from src.account import Account
+
 class Plan:
-    def __init__(self, config):
+    def __init__(self, config, accounts):
         self.config = config
+        self.accounts = accounts
 
     def get_growth(self):
         return self.config["average_growth"]
+
+    def get_header(self):
+        header_string = ""
+        for account in self.accounts:
+            header_string += account.get_name() + ","
+        
+        header_string = header_string.rstrip(',')
+        return header_string
+
+    def process_growth(self, years):
+        balances = []
+
+        for i in range(years+1):
+            balances.append([])
+            for account in self.accounts:
+                if i>=1:
+                    account.process_growth(self.config["average_growth"])
+                balances[i].append(account.get_balance())
+        return balances
+                
--- test/plan_test.py
+++ test/plan_test.py
@@ -1,10 +1,42 @@
 from src.plan import Plan
+from src.account import Account
 
+accounts = []
+accounts.append(Account({
+    "name": "Jerry's Roth",
+    "balance": 4000,
+    "annual_additions":2000,
+    "type": "Roth",
+    "withdrawl priority": 1,
+    "owner": "Jerry",
+    "trail_with_rmd": False
+}))
+accounts.append(Account({
+    "name": "Jill's Investment",
+    "balance": 10000,
+    "annual_additions": 5000,
+    "type": "Investment",
+    "withdrawl priority": 3,
+    "owner": "Jill",
+    "trail_with_rmd": False
+}))
+
+config = {
+    "average_growth": 6
+}
 
 def test_can_initialize_plan():
-    config = {
-        "average_growth": 6
-    }
-
-    plan = Plan(config)
+    plan = Plan(config, accounts)
     assert plan.get_growth() == 6
+
+def test_can_get_header():
+    plan = Plan(config, accounts)
+    assert plan.get_header() == "Jerry's Roth,Jill's Investment"
+
+def test_can_fill_table_for_one_year():
+    plan = Plan(config, accounts)
+    assert plan.process_growth(0) == [[4000, 10000]]
+
+def test_can_fill_table_for_two_years():
+    plan = Plan(config, accounts)
+    assert plan.process_growth(1) == [[4000, 10000], [6240.0, 15600.0]]
