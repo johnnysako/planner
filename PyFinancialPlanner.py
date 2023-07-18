1e302e7cb2b4708ccad73573c6f8b99f5b321b0c
John Chapman <superjohnnysako@gmail.com>
2023-07-18 08:40:49 -0400

    Some progress on making a pdf

--- PyFinancialPlanner.py
+++ PyFinancialPlanner.py
@@ -14,10 +14,14 @@ import json
 import csv
 import numpy as np
 import pandas as pd
+import datetime
 
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 years_to_process = 64
+iterations = 105
+remove_top = 5
 
 def load_constants():
     f = open('rmd.json')
@@ -41,32 +45,49 @@ def load_constants():
 
 def sort_data(data_for_analysis):
     sorted_data = sorted(data_for_analysis, key=lambda x: x.iloc[-1]['Sum of Accounts'], reverse=True)
-    return sorted_data[5:]
+    return sorted_data[remove_top:]
 
-def plot_year(data):
-    data.set_index('Year').plot.line()
-    plt.ticklabel_format(useOffset=False, style='plain')
-    plt.show()
+def plot_failed_plans(failed_plans, pdf):
+    for index, data in enumerate(failed_plans):
+        data.set_index('Year').plot.line()
+        plt.ticklabel_format(useOffset=False, style='plain')
+        plt.title('Failed Iteration ' + str(index+1))
+        pdf.savefig()
+        plt.close()
 
-def plot_results(data_for_analysis):
-    failed_plans = 0
+def plot_monte_carlo(data_for_analysis, failed_plans, pdf):
     for data in data_for_analysis:
         plt.plot(data['Year'], data['Sum of Accounts'])
         if data.iloc[-1]['Sum of Accounts']==0:
-            failed_plans+=1
+            failed_plans.append(data)
     
-    print("Failed Plans:", failed_plans)
     plt.ticklabel_format(useOffset=False, style='plain')
     plt.xlabel('Year')
     plt.ylabel('Net Worth')
-    plt.show()
+    plt.title('Monte Carlo Analysis')
+    pdf.savefig()
+    plt.close()
+
+    return failed_plans
+
+def plot_results(data_for_analysis):
+    with PdfPages('financial_analysis.pdf') as pdf:
+        failed_plans = []
+        plot_monte_carlo(data_for_analysis, failed_plans, pdf)
+
+        plot_failed_plans(failed_plans, pdf)
+
+        d = pdf.infodict()
+        d['Title'] = 'Financial Plan'
+        d['Author'] = u'John Chapman'
+        d['CreationDate'] = datetime.datetime.today()
 
 def main():
     rmd, tax, owners, expenses = load_constants()
 
     data_for_analysis = []
 
-    for i in range(10):
+    for i in range(iterations):
         rates = np.random.normal(6.0, 12.0, years_to_process+1)
 
         f = open('accounts.json')
