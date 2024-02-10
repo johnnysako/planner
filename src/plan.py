158ebdc4896cbcdac3982aaf3582a42d1d14dd57
John Chapman <superjohnnysako@gmail.com>
2024-02-10 08:10:22 -0500

    getting cleaner

--- src/plan.py
+++ src/plan.py
@@ -25,26 +25,26 @@ class Plan:
     def _all_owners_retired(self, year):
         return all(o.is_retired(year) for o in self.owners)
 
-    def _append_income(self, data, year):
+    def _append_income(self, i, year):
         income = 0
         for owner in self.owners:
             income += owner.get_income(self.config["Social Security"], year)
-        data.append(income)
+        self.balances[i].append(income)
         return income
 
-    def _append_rmd(self, data, year):
+    def _append_rmd(self, i, year):
         rmd = 0
         for account in self.accounts:
             owner = self._get_account_owner(account)
             rmd += account.withdraw_rmd(self.rmd.get_rate(
                 owner.get_age(year)), self.config['rmd'])
         rmd = round(rmd, 2)
-        data.append(rmd)
+        self.balances[i].append(rmd)
         return rmd
 
-    def _append_expenses(self, data, year, ):
+    def _append_expenses(self, i, year, ):
         value = self.expenses.get_expenses(year)
-        data.append(value)
+        self.balances[i].append(value)
         return value
 
     def _calculate_tax(self, year, rate, rmd):
@@ -58,21 +58,21 @@ class Plan:
         else:
             return 0
 
-    def _append_tax(self, data, year, rate, rmd):
+    def _append_tax(self, i, year, rate, rmd):
         tax = self._calculate_tax(year, rate, rmd)
-        data.append(tax)
+        self.balances[i].append(tax)
         return tax
 
-    def _append_reinvestment(self, data, income, tax, expense, rmd, year):
+    def _append_reinvestment(self, i, income, tax, expense, rmd, year):
         change = expense + tax - income - rmd
 
         if change < 0 and self._all_owners_retired(year):
             change = 0.0
 
-        data.append(max(-change, 0.0))
+        self.balances[i].append(max(-change, 0.0))
         return change
 
-    def _append_accounts(self, data, year, rate, change, growth):
+    def _append_accounts(self, i, year, rate, change, growth):
         total = 0
         for account in self.accounts:
             retired = self._owner_is_retired(account, year)
@@ -87,9 +87,26 @@ class Plan:
                     change -= balance
                     account.withdraw(balance)
             balance = account.get_balance()
-            data.append(balance)
+            self.balances[i].append(balance)
             total += balance
         return round(total, 2)
+
+    def _append_rates(self, i, rates, year):
+        if self._bad_timing(year):
+            rates["s"][i] = -37
+            rates["b"][i] = 13.4
+
+        self.balances[i].append(rates["s"][i])
+        self.balances[i].append(rates["b"][i])
+
+        return {"s": rates["s"][i], "b": rates["b"][i]}
+
+    def _append_percent_pulled(self, i, change, total):
+        if change > 0 and total > 0:
+            self.balances[i].append(round(change/total*100, 2))
+        else:
+            self.balances[i].append(0.0)
+        self.balances[i].append(total)
 
     def _bad_timing(self, year):
         retirement_year = 65535
@@ -116,33 +133,20 @@ class Plan:
 
     def process_plan(self, years, rates):
 
-        for i, year in enumerate(range(self.start_year, 
+        for plan_iteration, year in enumerate(range(self.start_year, 
                                        self.start_year + years + 1)):
             self.balances.append([])
-            self.balances[i].append(year)
+            self.balances[plan_iteration].append(year)
 
-            if self._bad_timing(year):
-                rates["s"][i] = -37
-                rates["b"][i] = 13.4
-
-            self.balances[i].append(rates["s"][i])
-            self.balances[i].append(rates["b"][i])
-
-            rate = {"s": rates["s"][i], "b": rates["b"][i]}
-
-            income = self._append_income(self.balances[i], year)
-            rmd = self._append_rmd(self.balances[i], year)
-            expense = self._append_expenses(self.balances[i], year)
-            tax = self._append_tax(self.balances[i], year, rate, rmd)
-            change = self._append_reinvestment(self.balances[i], income,
+            rate = self._append_rates(plan_iteration, rates, year)
+            income = self._append_income(plan_iteration, year)
+            rmd = self._append_rmd(plan_iteration, year)
+            expense = self._append_expenses(plan_iteration, year)
+            tax = self._append_tax(plan_iteration, year, rate, rmd)
+            change = self._append_reinvestment(plan_iteration, income,
                                                tax, expense, rmd, year)
-
-            total = self._append_accounts(self.balances[i], year,
-                                          rate, change, i != 0)
-            if change > 0 and total > 0:
-                self.balances[i].append(round(change/total*100, 2))
-            else:
-                self.balances[i].append(0.0)
-            self.balances[i].append(total)
+            total = self._append_accounts(plan_iteration, year,
+                                          rate, change, plan_iteration != 0)
+            self._append_percent_pulled(plan_iteration, change, total)
 
         return self.balances
