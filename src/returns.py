c1d66265914c5dfef21bcceb6e2e0056ac152af6
John Chapman <superjohnnysako@gmail.com>
2024-07-20 07:54:24 -0400

    Generated a file with 150 years of returns and 1000 data points

--- src/returns.py
+++ src/returns.py
@@ -1,4 +1,7 @@
 import numpy as np
+import json
+import os
+import yfinance as yf
 
 
 def generate_returns(data_distribution, mean, std, years_to_process):
@@ -17,3 +20,55 @@ def generate_returns(data_distribution, mean, std, years_to_process):
                            .uniform(data_distribution[random-1],
                                     data_distribution[random]))
     return np.array(returns)
+
+
+def create_data_table(distribution, mean, std,
+                      years_to_process, num_simulations, file_name):
+    all_returns = []
+    for _ in range(num_simulations):
+        returns = generate_returns(distribution,
+                                   mean, std, years_to_process)
+        all_returns.append(returns.tolist())
+
+    with open(os.path.join('_internal',
+                           file_name), 'w') as f:
+        json.dump(all_returns, f)
+
+
+def create_stock_returns():
+    symbol = "^GSPC"
+    start_date = "1950-01-01"
+    end_date = "2021-12-31"
+
+    data = yf.download(symbol, start=start_date, end=end_date)
+
+    stock_annual_returns = data['Adj Close'].resample(
+        'Y').ffill().pct_change().dropna()
+    sorted_stock_annual_returns = sorted(stock_annual_returns)
+
+    create_data_table(sorted_stock_annual_returns, 26, 9, 150, 1000,
+                      'stock_returns.json')
+
+
+def create_bond_returns():
+    symbol = "LQD"
+    start_date = "2002-07-29"
+    end_date = "2021-12-31"
+
+    data = yf.download(symbol, start=start_date, end=end_date)
+
+    bond_annual_returns = data['Adj Close'].resample(
+        'Y').ffill().pct_change().dropna()
+    sorted_bond_annual_returns = sorted(bond_annual_returns)
+
+    create_data_table(sorted_bond_annual_returns, 6, 2, 150, 1000,
+                      'bond_returns.json')
+
+
+def main():
+    create_stock_returns()
+    create_bond_returns()
+
+
+if __name__ == "__main__":
+    main()
